7680f6eef3814341a662b0a4ed1a1679a65ba46f
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-11-03 13:18:08 -0600

    Firebase setup pt1

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDPN3N5Esg7GgVGRhpiL3vX5jiKwlQCt6c",
+    authDomain: "firegram-eeb4c.firebaseapp.com",
+    databaseURL: "https://firegram-eeb4c.firebaseio.com",
+    projectId: "firegram-eeb4c",
+    storageBucket: "firegram-eeb4c.appspot.com",
+    messagingSenderId: "251847812139",
+    appId: "1:251847812139:web:811d94aa247fbb4444b66e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
