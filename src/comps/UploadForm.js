5b52f6e1882f9756026549b15afe84f540c54ff1
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-11-03 14:04:15 -0600

    upload logic handling

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
+    //useState hook to store in local state
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    //Allowed file types
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        //console.log('CHANGED');
+
+        //first file selected from e
+        let selected = e.target.files[0];
+        //console.log(selected);
+
+        if (selected && types.includes(selected.type)){
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError('Select valid file type (PNG or JPEG)')
+
+        }
+    }
 
     return (
         <form>
-            <input type='file'/>
+            <input type='file' onChange={changeHandler} />
+            <div className='output' >
+                {error && <div className='error' > { error } </div> }
+            </div>
         </form>
     )
 
