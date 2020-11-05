5db0ce186262102b0a9c5816600d21f5f7e3d54d
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-11-05 10:17:18 -0600

    tweaks

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Patty Martirosian</h1>
+      <h2>Gallery</h2>
+      <p>Previous/current/upcoming pieces...</p>
     </div>
   )
 }
